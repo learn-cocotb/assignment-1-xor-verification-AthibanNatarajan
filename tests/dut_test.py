edfdd3a6fdd4babf423e81210f58d7d4a60056f2
AthibanNatarajan <74203408+AthibanNatarajan@users.noreply.github.com>
2023-05-29 00:38:21 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,7 +1,14 @@
 import cocotb
 from cocotb.triggers import Timer
 
-
 @cocotb.test()
 async def dut_test(dut):
-    assert 0, "Test Not Implemented Error"
+	a=(0,0,1,1);
+	b=(0,1,0,1);
+	op=(0,1,1,0);
+	for i in range(4):
+		dut.a.value=a[i];
+		dut.b.value=b[i];
+		await Timer(1, 'ns')
+		assert dut.op.value==op[i], f" Failed, Error in XOR output in iteration {i}"
+
